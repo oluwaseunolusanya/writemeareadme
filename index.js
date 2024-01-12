10e19204f8da679eb45a3918cd4469de8f4ece9c
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-01-12 22:16:27 +0000

    Updated 'index.js' with required modules and array declaration.

---
+++ index.js
@@ -0,0 +1,11 @@
+// Import Required Modules.
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// Declare an Array of User Prompts
+const questions = [
+    {}
+];
+
