05c7110009ca84c9130b9791b718ae8c3d46e627
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-01-12 21:58:04 +0000

    Defined README markdowns in the 'generateMarkdown' function.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,26 @@
 // Function to generate markdowns for the README
 const generateMarkdown = (data) => {
-    `${data.title}
+    `# ${data.title}
+    ## Description
+    ${data.description} 
+    ## Table of Contents 
+    [Description](#description)
+    [Installation](#installation)
+    [Usage](#usage) 
+    [License](#license)
+    [Contributing](#contributing) 
+    [Tests](#tests) 
+    [Questions](#questions)
+
+    ## Installation 
+    ${data.description} 
+
+    ## Usage
+    ${data.description} 
+    
+    ## License 
+    ## Contributing 
+    ## Tests 
+    ## Questions
     `
 }
