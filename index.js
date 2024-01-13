5ce214670e3e01cbe4b1f2d5db900ca7159512f7
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-01-13 20:38:13 +0000

    Downgraded 'inquirer' from version 9 and define a user prompts.

--- index.js
+++ index.js
@@ -6,6 +6,30 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // Declare an Array of User Prompts
 const questions = [
-    {}
+    {
+        type:"input",
+        message: "Enter the title of your project.",
+        name: "title"
+    },
+    {
+        type:"input",
+        message: "Enter a concise description of the project.",
+        name: "description"
+    },
+    {
+        type:"input",
+        message: "Provide installation instructions.",
+        name: "installation"
+    },
+    {
+        type:'input',
+        message: 'Describe how to use the program.',
+        name: 'usage'
+    },
+    {
+        type:'input',
+        message: ''.',
+        name: 'description'
+    },
 ];
 
