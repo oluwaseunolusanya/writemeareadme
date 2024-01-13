d47534617f65ea66c54ad246313fd770b9404a0f
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-01-13 21:15:24 +0000

    Updated the array of user prompts with data.

--- index.js
+++ index.js
@@ -22,14 +22,53 @@ const questions = [
         name: "installation"
     },
     {
-        type:'input',
+        type:"input",
         message: 'Describe how to use the program.',
         name: 'usage'
     },
     {
-        type:'input',
-        message: ''.',
-        name: 'description'
+        type:"list",
+        message: "Choose a license to open source your repository.",
+        name: "License"
+    },
+    {
+        type:"input",
+        message: "Choose a license to open source your repository.",
+        name: "License",
+        choices:[
+            "Apache 2.0 License",
+            "Boost Software License 1.0",
+            "BSD 3-Clause License",
+            "BSD 2-Clause License",
+            "CC0",
+            "CC0-1.0",
+            "Attribution 4.0 International",
+            "Attribution-ShareAlike 4.0 International",
+            "Attribution-NonCommercial 4.0 International",
+            "Attribution-NoDerivates 4.0 International",
+            "Attribution-NonCommmercial-ShareAlike 4.0 International",
+            "Attribution-NonCommercial-NoDerivatives 4.0 International",
+            "Eclipse Public License 1.0",
+            "GNU GPL v3",
+            "GNU GPL v2",
+            "GNU AGPL v3",
+            "GNU LGPL v3",
+            "GNU FDL v1.3",
+            "Hippocratic 2.1 ",
+            "Hippocratic 3.0 ",
+            "IBM Public License Version 1.0",
+            "ISC License (ISC)",
+            "MIT",
+            "Mozilla Public License 2.0",
+            "Attribution License (BY)",
+            "Open Database License (ODbL)",
+            "Public Domain Dedication and License (PDDL)",
+            "Perl Artistic-2.0",
+            "SIL Open Font License 1.1",
+            "Unlicense",
+            "WTFPL",
+            "Zlib",
+        ]
     },
 ];
 
