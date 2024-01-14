9bac14e72cdcf71f12912babfa2b2f7689772d7d
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-01-14 16:49:03 +0000

    Added license notice to 'generateMarkdown' function.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,11 @@
+// Get the year
+const date = new Date();
+let year = date.getFullYear();
+
 // Function to generate markdowns for the README
 const generateMarkdown = (data) => {
     return `# ${data.title}
-    ## Description
+    ## Description                       ${generateBadge(data.license)}
     ${data.description} 
     ## Table of Contents 
     [Description](#description)
@@ -20,6 +24,13 @@ const generateMarkdown = (data) => {
 
     ## License 
     ${data.license}
+
+    Copyright (c) ${year} ${npm_package_author}
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
     ## Contributing 
     ${data.contributing}
