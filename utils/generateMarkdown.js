625a92b1b91a35eaa76d071b0c79b457d4359f15
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-01-14 22:09:09 +0000

    Realign content of generateMarkdown function.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -142,42 +142,42 @@ const generateBadge = (license) => {
 // Function to generate markdowns for the README
 const generateMarkdown = (data) => {
     return `# ${data.title}                   ${generateBadge(data.license)}
-    ## Description                       
-    ${data.description} 
-    ## Table of Contents 
-    [Description](#description)
-    [Installation](#installation)
-    [Usage](#usage) 
-    [License](#license)
-    [Contributing](#contributing) 
-    [Tests](#tests) 
-    [Questions](#questions)
+## Description                       
+${data.description} 
+## Table of Contents 
+[Description](#description)
+[Installation](#installation)
+[Usage](#usage) 
+[License](#license)
+[Contributing](#contributing) 
+[Tests](#tests) 
+[Questions](#questions)
 
-    ## Installation 
-    ${data.installation} 
+## Installation 
+${data.installation} 
 
-    ## Usage
-    ${data.usage} 
+## Usage
+${data.usage} 
 
-    ## License 
-    ${data.license}
+## License 
+${data.license}
 
-    Copyright (c) ${year} ${data.author}
+Copyright (c) ${year} ${data.author}
     
-    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
     
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-    ## Contributing 
-    ${data.contributing}
+## Contributing 
+${data.contributing}
     
-    ## Tests
-    ${data.tests}
+## Tests
+${data.tests}
 
-    ## Questions
-    Please engage with me on https://github.com/${data.githubusername} if you have questions about the project. You can also reach me at ${data.email}.
-    `
+## Questions
+Please engage with me on https://github.com/${data.githubusername} if you have questions about the project. You can also reach me at ${data.email}.
+`
 }
 
 module.exports = generateMarkdown;
